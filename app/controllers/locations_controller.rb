c989a2cc168ae2f8dc512b782cdb864711d4f990
timmtonga <timmtonga@gmail.com>
2018-02-08 10:11:55 -0500

    Fixed syntax error in retrieving locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   def suggestions
-    workstation_id = LocationTag.select(:location_tag_id).where(name: ['workstation location'])
+    workstation_id = LocationTag.select(:location_tag_id).where(name: 'workstation location').first
     locations = workstation_id.location_tag_map.collect{|x| x.location_id}
     names = Location.select(:name, :location_id).where("name LIKE '%#{params[:search_string]}%'
                                                         AND location_id in (?)", locations).map do |v|
